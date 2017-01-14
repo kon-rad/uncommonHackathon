2d66d2056b4d6a53f04d9662688aed5fc5a057a9
Fernando1Borges <centraloro@gmail.com>
2017-01-14 16:36:24 -0600

    Add updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+//
+// $.ajax({
+//         url: "https://api.spotify.com/v1/tracks/3n3Ppam7vgaVa1iaRUc9Lp",
+//         type:"GET",
+//         async:false,
+//         dataType: "json",
+//         success:  $.getJSON(function(data) {
+//         alert(data);
+//       console.log(data);
+//     });
+//     });
